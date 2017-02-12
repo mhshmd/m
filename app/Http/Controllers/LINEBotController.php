e468e315dfe645c6a1018c49452ee528bc3ec49a
muhshamad <13.7741@stis.ac.id>
2017-02-13 00:27:14 +0700

    LINEBot beta 1

--- app/Http/Controllers/LINEBotController.php
+++ app/Http/Controllers/LINEBotController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 #Library
 //LINEBot
 use App\Libraries\LINE\LINEBot;
+use App\Libraries\LINE\SignatureValidator;
 use App\Libraries\LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 #DB
@@ -18,11 +19,52 @@ class LINEBotController extends Controller
 	public function bot(Request $request)
     {
 
-    	$post = $request->all();
+    	// get request body and line signature header
 
-    	$httpClient = new CurlHTTPClient('<channel access token>');
+    	//$body = $request->all();
 
-		$bot = new LINEBot($httpClient, ['channelSecret' => 'c1e97f1d72e19a6d30302ada807611e1']);
+    	$body = file_get_contents('php://input')
+
+    	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+    	// is LINE_SIGNATURE exists in request header?
+
+		if (empty($signature)){
+
+			abort(400, 'Signature not set');
+
+		}
+
+		// is this request comes from LINE?
+		if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+
+		    abort(400, 'Invalid signature');
+
+		}
+
+    	$httpClient = new CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+
+		$bot = new LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
+		$data = json_decode($body, true);
+
+		foreach ($data['events'] as $event) {
+
+		    if ($event['type'] == 'message') {
+
+			    if($event['message']['type'] == 'text') {
+
+			        // send same message as reply to user
+
+			        $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+					return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+
+			    }
+
+		    }
+
+		}
 
     	return $post;
 
